fc18544391357d2d5a56f6f7bbadd33881e2a152
Saad Ahmed <saadahm.siddiqui@gmail.com>
2023-03-29 21:31:40 +0500

    Separate implementation of tokens from BridgeToken

--- packages/core/src/lib/chains/evm/types.ts
+++ packages/core/src/lib/chains/evm/types.ts
@@ -1,45 +1,48 @@
-import { ethers } from "ethers";
-import { BridgeEvmNetworks } from "../../common/networks/networks";
-import { BridgeToken } from "../..//common";
-export type TokenConfig = {
-    symbol: string;
-    name: string;
-    decimals: number;
-    address: string;
+import {ethers} from "ethers";
+import {BridgeEvmNetworks} from "../../common/networks/networks";
+
+export type EvmTokenConfig = {
+  symbol: string;
+  name: string;
+  decimals: number;
+  address: string;
+  bridgeConfig: {
+    minTransferAmount: number;
+  };
 };
 
 export type EvmConfig = {
-    [network in BridgeEvmNetworks]: {
-        chainId: number;
-        bridge: string;
-        rpcUrl: string;
-        tokens: BridgeToken[];
-        depositWallet: string;
-        releaseWallet: string;
-    };
+  [network in BridgeEvmNetworks]: {
+    chainId: number;
+    bridge: string;
+    rpcUrl: string;
+    tokens: EvmTokenConfig[];
+    depositWallet: string;
+    releaseWallet: string;
+  };
 };
 
 export type BridgeDepositEvent = {
-    amount: ethers.BigNumber;
-    destinationChainId: number;
-    destinationWallet: string;
-    erc20Address: string;
-    __type: "BridgeDeposit";
+  amount: ethers.BigNumber;
+  destinationChainId: number;
+  destinationWallet: string;
+  erc20Address: string;
+  __type: "BridgeDeposit";
 };
 
 export type BridgeReleaseEvent = {
-    amount: ethers.BigNumber;
-    depositTransactionHash: string;
-    destinationWallet: string;
-    erc20Address: string;
-    __type: "BridgeRelease";
+  amount: ethers.BigNumber;
+  depositTransactionHash: string;
+  destinationWallet: string;
+  erc20Address: string;
+  __type: "BridgeRelease";
 };
 
 export type TransferEvent = {
-    from: string;
-    to: string;
-    value: ethers.BigNumber;
-    __type: "Transfer";
+  from: string;
+  to: string;
+  value: ethers.BigNumber;
+  __type: "Transfer";
 };
 
 export type EvmNetworkConfig = EvmConfig[BridgeEvmNetworks];
