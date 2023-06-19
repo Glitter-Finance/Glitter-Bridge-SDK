3dfac2f8e2b8385a845488db3cad34e6531afae2
Andrew Carl <andrewc@glitter.finance>
2023-06-19 16:53:06 -0400

    Final solana schema fix

--- packages/sdk-core/src/index.ts
+++ packages/sdk-core/src/index.ts
@@ -1,4 +1,9 @@
+import { LoadSolanaSchema } from "./lib";
+
 export * from "./GlitterBridgeSDK"
 export * from "./config"
 export * from "./types"
 export * from "./lib"
+
+//Needed to use the static schema downstream
+LoadSolanaSchema();
