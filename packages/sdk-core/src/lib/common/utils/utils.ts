d20df79e18727f3086dd70fc9a7b9fe3544fa1fe
aurel-fr <105201452+aurel-fr@users.noreply.github.com>
2023-06-23 12:24:51 +0800

    refactor: type for instanceofAlgoAccount

--- packages/sdk-core/src/lib/common/utils/utils.ts
+++ packages/sdk-core/src/lib/common/utils/utils.ts
@@ -46,7 +46,7 @@ export const base64ToBigUIntString = (encoded: any) => {
     return Buffer.from(encoded, "base64").readBigUInt64BE().toString();
 };
 
-export function instanceofAlgoAccount(account: any): boolean {
+export function instanceofAlgoAccount(account: any): account is algosdk.Account {
     if(typeof account !=="object" || account == null) return false
     return "addr" in account && "sk" in account && isValidAddress(account.addr);
 }
@@ -72,7 +72,7 @@ export function walletToAddress(wallet: string | PublicKey | algosdk.Account): s
 
 export const addr_to_pk=(addr:string| PublicKey | algosdk.Account):Buffer=>{
     if (addr instanceof PublicKey) return addr.toBuffer();
-    if (instanceofAlgoAccount(addr)) return Buffer.from(decodeAddress((addr as algosdk.Account).addr).publicKey);
+    if (instanceofAlgoAccount(addr)) return Buffer.from(decodeAddress(addr.addr).publicKey);
     if(typeof addr !=="string") throw new Error("unsupported address for bridge v2") ;
     if(addr.startsWith("0x")) return Buffer.from(addr.slice(2), "hex");
     return Buffer.from(addr, "hex");
