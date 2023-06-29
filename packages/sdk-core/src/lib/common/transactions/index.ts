6467e275dca7490fc5bee5e9eacb60a5690a9793
Andrew Carl <andrewc@glitter.finance>
2023-06-28 20:00:42 -0400

    Added PartialBridgeTxnEquals

--- packages/sdk-core/src/lib/common/transactions/index.ts
+++ packages/sdk-core/src/lib/common/transactions/index.ts
@@ -51,3 +51,31 @@ export type PartialTxn = {
     referral_id?: number | null;
     gasPaid?: BigNumber | null;
 };
+
+export function PartialBridgeTxnEquals(a: PartialBridgeTxn, b: PartialBridgeTxn): boolean {
+    const differences = [];
+    if (a.txnID !== b.txnID) differences.push("txnID");
+    if (a.txnIDHashed !== b.txnIDHashed) differences.push("txnIDHashed");
+    if (a.bridgeType !== b.bridgeType) differences.push("bridgeType");
+    if (a.txnTimestamp !== b.txnTimestamp) differences.push("txnTimestamp");
+    if (a.block !== b.block) differences.push("block");
+    //if (a.confirmations !== b.confirmations) differences.push("confirmations");
+    if (a.txnType !== b.txnType) differences.push("txnType");
+    if (a.chainStatus !== b.chainStatus) differences.push("chainStatus");
+    if (a.network !== b.network) differences.push("network");
+    if (a.tokenSymbol !== b.tokenSymbol) differences.push("tokenSymbol");
+    if (a.baseSymbol !== b.baseSymbol) differences.push("baseSymbol");
+    if (a.address !== b.address) differences.push("address");
+    if (a.units !== b.units) differences.push("units");
+    if (a.amount !== b.amount) differences.push("amount");
+    if (a.routing !== b.routing) differences.push("routing");
+    if (a.protocol !== b.protocol) differences.push("protocol");
+    if (a.referral_id !== b.referral_id) differences.push("referral_id");
+    if (a.gasPaid !== b.gasPaid) differences.push("gasPaid");
+    
+    if (differences.length > 0) {
+        console.log("PartialBridgeTxnEquals: differences", differences);
+        return false;
+    }
+    return true;
+}
